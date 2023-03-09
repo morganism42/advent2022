da8cae2e1c9b6901d87f5c019b4d849c85d016fd
morganism42 <liamstregger@gmail.com>
2023-03-09 15:17:34 -0800

    update

--- day12.py
+++ day12.py
@@ -1,4 +1,6 @@
-abaaaaaaaaccccccccccccccccccaaaaaccccaaaaaaccccccccccccccccccccccaaaaaaaaaacccccccccccccccccccccccccccccccaaaaaccccccccccccccccccccccccccccccccccccccccccaaaaaa
+from math import inf
+
+data = '''abaaaaaaaaccccccccccccccccccaaaaaccccaaaaaaccccccccccccccccccccccaaaaaaaaaacccccccccccccccccccccccccccccccaaaaaccccccccccccccccccccccccccccccccccccccccccaaaaaa
 abaaaaaaaacccccccccccccccccccaaaaaccccaaaacccccaaaacccccccccccccccaaaaaaaaaacccccccccccccccccccccccccccccaaaaaaccccccccccccccccccccccccccccccccccccccccccccaaaa
 abccaaaaaaccccccccccccccccccaaaaaaccccaaaaccccaaaaaccccccccccaaaaaaaaaaaaaaacccccccccccccccccccccccccccccaaaacccccccccccccccccccccccccccccaaaccccccccccccccaaaa
 abcaaaaaaaccccccccccccccccccaaaaccccccaccaccccaaaaaacccccccccaaaaaaaaaaaaaaacccccccccccccccccccccacccccccccaacccccccccccccccccccccccccccccaaaccccccccccccccaaaa
@@ -38,4 +40,59 @@ abacccaaaaaaaaccccccccaaacccccccccaaaaaaccccccccccccccccccccccaaacaaaacaaaacccca
 abcccccccaaacccccccccaaaaaaccccccaaaaaaaacccccccccccccccccccaaaaaaaaaaaaaaaccccaaaaaaccccccacaaaacccccccaaaaacccccccaaaaaccccccccccccaaacgggggaccccccccccccccaa
 abcccccccaaccccccccccaaaaaaccccccaaaaaaaacccccccaaacccccccccaaaaaaaaaaaaaaaacccaaaaaaccccccaaaaaaccccccaaaaaaccccccaaaaaaacccccccccccaaaccccaaaccccccccccaaacaa
 abcccccccccccccccccccaaaaaccccccccccaaccccccccaaaaaccccccccccaaaaaaaaaaaaaaaaccccaaaccccccccaaaacccccccaaaaccccccccccccaaccccccccccccccccccccccccccccccccaaaaaa
-abccccccccccccccccccccaaaaacccccccccaaccccccccaaaaaacccccccccaaaaaaaaaaaaaaaacccccccccccccccaaaacccccccccaacccccccccccccccccccccccccccccccccccccccccccccccaaaaa
+abccccccccccccccccccccaaaaacccccccccaaccccccccaaaaaacccccccccaaaaaaaaaaaaaaaacccccccccccccccaaaacccccccccaacccccccccccccccccccccccccccccccccccccccccccccccaaaaa'''
+area = [[]]
+for x in data:
+    if x == 'S':
+        area[-1].append('S')
+    elif x == 'E':
+        area[-1].append('E')
+    elif x == '\n':
+        area.append([])
+    else:
+        area[-1].append(ord(x) - 96)
+for x, i in enumerate(area):
+    for y, j in enumerate(i):
+        if j == 'S':
+            area[x][y] = 1
+            path = [[x, y]]
+        elif j == 'E':
+            goal = [x, y]
+
+global sstep
+global bad
+bad = []
+sstep = inf
+
+
+def nextstep(path, step):
+    global sstep
+    global bad
+    step += 1
+    steppx = 0
+    steppy = 0
+    stepnx = 0
+    stepny = 0
+    if path[-1] == goal and step < sstep:
+        sstep = step
+        return step
+    if path[-1] in bad:
+        return inf
+    if path[-1][0] + 1 < len(area):
+        if [path[-1][0] + 1, path[-1][1]] not in path and area[path[-1][0] + 1][path[-1][1]] <= area[path[-1][0]][path[-1][1]] + 1:
+            steppx = nextstep(path + [[path[-1][0] + 1, path[-1][1]]], step)
+    if (path[-1][1] + 1) < len(area[0]):
+        if [path[-1][0], path[-1][1] + 1] not in path and area[path[-1][0]][path[-1][1] + 1] <= area[path[-1][0]][path[-1][1]] + 1:
+            steppy = nextstep(path + [[path[-1][0], path[-1][1] + 1]], step)
+    if path[-1][0] - 1 > 0:
+        if [path[-1][0] - 1, path[-1][1]] not in path and area[path[-1][0] - 1][path[-1][1]] <= area[path[-1][0]][path[-1][1]] + 1:
+            stepnx = nextstep(path + [[path[-1][0] - 1, path[-1][1]]], step)
+    if path[-1][1] - 1 > 0:
+        if [path[-1][0], path[-1][1] - 1] not in path and area[path[-1][0]][path[-1][1] - 1] <= area[path[-1][0]][path[-1][1]] + 1:
+            stepny = nextstep(path + [[path[-1][0], path[-1][1] - 1]], step)
+    step += min([stepny, steppy, stepnx, steppx])
+    bad.append(path[-1])
+    return step
+
+
+print(nextstep(path, 0))
--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-import day11
+import day10
 
